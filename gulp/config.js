a418c9cc7336c94505124e78b80da4ae19cf5fa9
Christian Acuna <cacuna0828@gmail.com>
2016-09-24 14:52:33 +0800

    add images.js task

--- gulp/config.js
+++ gulp/config.js
@@ -3,7 +3,10 @@ var dist              = 'dist';
 var public            = 'public';
 // var production        = 'build/production';
 // var srcAssets         = 'app/_assets';
-// var developmentAssets = 'build/assets';
+var developmentImages = 'dist/images';
+var developmentStyles = 'dist/styles';
+var mainPic = 'src/images/main/christian_acuna.jpg';
+var portfolioImages = 'src/images/*.{jpg,png}';
 // var productionAssets  = 'build/production/assets';
 
 module.exports = {
@@ -14,8 +17,81 @@ module.exports = {
       },
       files: [
         src + '/css/*.css',
-        src + '/js/*.js'
+        src + '/js/*.js',
+        'index.html'
       ]
     }
+  },
+  delete: {
+    src: [developmentImages, developmentStyles]
+  },
+  images: {
+    me: mainPic,
+    imageConfig: {
+      // Convert all images to JPEG format
+      '*': [{
+        width: 350,
+        rename: {
+          extname: '.jpg',
+        },
+      }, {
+        width: 350 * 2,
+        rename: {
+          suffix: '@2x',
+          extname: '.jpg',
+        },
+      }, {
+        width: 350,
+        rename: {
+          extname: '.webp'
+        },
+      }, {
+        width: 350 * 2,
+        rename: {
+          suffix: '@2x',
+          extname: '.webp',
+        },
+      }],
+    },
+    imageFolder: developmentImages,
+    portImages: portfolioImages,
+    portImagesConfig: {
+      // Convert all images to JPEG format
+      '*': [{
+        width: 500,
+        rename: {
+          extname: '.jpg',
+        },
+      }, {
+        width: 500 * 2,
+        rename: {
+          suffix: '@2x',
+          extname: '.jpg',
+        },
+      }, {
+        width: 500 * 3,
+        rename: {
+          suffix: '@3x',
+          extname: '.jpg',
+        },
+      }, {
+        width: 500,
+        rename: {
+          extname: '.webp'
+        },
+      }, {
+        width: 500 * 2,
+        rename: {
+          suffix: '@2x',
+          extname: '.webp',
+        },
+      }, {
+        width: 500 * 3,
+        rename: {
+          suffix: '@3x',
+          extname: '.webp',
+        },
+      }],
+    },
   }
 };
